033bc1483d11749a697c58ff592408c2f2f4b3aa
Melissa Pabst <melissapabst@gmail.com>
2018-05-26 20:26:06 -0500

    change searchTerm/Type to variables

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -28,15 +32,15 @@ public class SearchController {
         if(searchType.equals("all")) {
             ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
             model.addAttribute("columns", ListController.columnChoices);
-            model.addAttribute("searchType" = searchType);
-            model.addAttribute("searchTerm" = searchTerm);
+            model.addAttribute(searchType = searchType);
+            model.addAttribute(searchTerm = searchTerm);
             model.addAttribute("jobs", jobs);
 
         } else {
             ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchTerm, searchType);
             model.addAttribute("columns", ListController.columnChoices);
-            model.addAttribute("searchType" = searchType);
-            model.addAttribute("searchTerm" = searchTerm);
+            model.addAttribute(searchType = searchType);
+            model.addAttribute(searchTerm = searchTerm);
             model.addAttribute("jobs", jobs);
         }
         return "search";
